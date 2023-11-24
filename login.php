54ff44464642500d840b94c99a584d1b3d550d23
Xyrex Kyle Salazar <122286060+xyxyxyrex@users.noreply.github.com>
2023-11-24 11:02:43 +0800

    Update login.php

--- login.php
+++ login.php
@@ -10,14 +10,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
 
-$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$sql = "SELECT * FROM users WHERE username='$username'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "Login successful";
+    $row = $result->fetch_assoc();
+    $hashed_password = $row['password_hash'];
+
+    // Use password_verify to check if the entered password matches the hashed password
+    if (password_verify($password, $hashed_password)) {
+        echo "Login successful";
+    } else {
+        echo "Login failed";
+    }
 } else {
     echo "Login failed";
 }
