7a873a294cee4157598a7ab11e2104954bd2550e
xyxyxyrex <122286060+xyxyxyrex@users.noreply.github.com>
2023-11-27 09:29:17 +0800

    added register function

--- app/src/main/java/com/example/thriftlyapp/LoginActivity.java
+++ app/src/main/java/com/example/thriftlyapp/LoginActivity.java
@@ -1,14 +1,16 @@
 package com.example.thriftlyapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -37,19 +39,48 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Perform login
-                String loginUrl = "http://localhost/phpmyadmin/thriftlyMobile/login.php";
-                String loginData = "username=" + username.getText().toString() +
-                        "&password=" + password.getText().toString();
-                String response = HttpUtility.sendHttpRequest(loginUrl, loginData);
+                String usernameText = username.getText().toString();
+                String passwordText = password.getText().toString();
 
-                // Process the response
-                if (response != null && response.equals("Login successful")) {
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                if (validateInputs(usernameText, passwordText)) {
+                    new LoginTask().execute(usernameText, passwordText);
                 } else {
-                    // Handle login failure, for example, show an error message
-                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    // Handle invalid input, for example, show an error message
+                    Toast.makeText(LoginActivity.this, "Invalid input. Please check your inputs and try again.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private boolean validateInputs(String username, String password) {
+        // Add your input validation logic here
+        return !username.isEmpty() && !password.isEmpty();
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String loginUrl = "http://192.168.55.178/thriftlyMobile/login.php";
+                String loginData = "username=" + params[0] +
+                        "&password=" + params[1];
+                return HttpUtility.sendHttpRequest(loginUrl, loginData);
+            } catch (Exception e) {
+                return "Error: " + e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            Log.d("LoginActivity", "Server response: " + response);
+            Log.d("LoginActivity", "Response length: " + response.length());
+
+            if (response != null && response.contains("Login successful")) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            } else {
+                // Handle login failure, for example, show an error message
+                Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/thriftlyapp/RegisterActivity.java
+++ app/src/main/java/com/example/thriftlyapp/RegisterActivity.java
@@ -1,13 +1,13 @@
 package com.example.thriftlyapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,12 +26,11 @@ public class RegisterActivity extends AppCompatActivity {
         password = findViewById(R.id.password_reg);
         confirmpass = findViewById(R.id.confirmpass_reg);
         mobilenum = findViewById(R.id.mobilenum_reg);
-        signIn = findViewById(R.id.sign_in); // Initialize the signIn TextView
+        signIn = findViewById(R.id.sign_in);
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Start LoginActivity
                 startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
             }
         });
@@ -39,16 +38,45 @@ public class RegisterActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Perform registration
-                String registerUrl = "http://192.168.55.178/thriftlyMobile/register.php";
-                String registrationData = "username=" + username.getText().toString() +
-                        "&email=" + email.getText().toString() +
-                        "&password=" + password.getText().toString() +
-                        "&confirm_password=" + confirmpass.getText().toString() +
-                        "&mobile_number=" + mobilenum.getText().toString();
-                String response = HttpUtility.sendHttpRequest(registerUrl, registrationData);
+                String usernameText = username.getText().toString();
+                String emailText = email.getText().toString();
+                String passwordText = password.getText().toString();
+                String confirmPasswordText = confirmpass.getText().toString();
+                String mobileNumberText = mobilenum.getText().toString();
 
-                // Process the response
+                if (validateInputs(usernameText, emailText, passwordText, confirmPasswordText, mobileNumberText)) {
+                    new RegisterTask().execute(usernameText, emailText, passwordText, confirmPasswordText, mobileNumberText);
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Invalid input. Please check your inputs and try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean validateInputs(String username, String email, String password, String confirmPassword, String mobileNumber) {
+        // Add your input validation logic here
+        return !username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPassword.isEmpty() && !mobileNumber.isEmpty();
+    }
+
+    private class RegisterTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String registerUrl = "http://192.168.55.178/thriftlyMobile/register.php";
+                String registrationData = "username=" + params[0] +
+                        "&email=" + params[1] +
+                        "&password=" + params[2] +
+                        "&confirm_password=" + params[3] +
+                        "&mobile_number=" + params[4];
+                return HttpUtility.sendHttpRequest(registerUrl, registrationData);
+            } catch (Exception e) {
+                return "Error: " + e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            if (response != null) {
                 if (response.equals("Registration successful")) {
                     Toast.makeText(RegisterActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
                 } else if (response.equals("Passwords do not match")) {
@@ -56,10 +84,11 @@ public class RegisterActivity extends AppCompatActivity {
                 } else if (response.equals("Error: Email already registered")) {
                     Toast.makeText(RegisterActivity.this, "Error: Email already registered", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(RegisterActivity.this, "Error: Registration failed", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Error: Registration failed - " + response, Toast.LENGTH_SHORT).show();
                 }
+            } else {
+                Toast.makeText(RegisterActivity.this, "Error: Null response received", Toast.LENGTH_SHORT).show();
             }
-        });
-
+        }
     }
 }
