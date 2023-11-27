2107d0483271d2c2c69043f6a806ce0310a7b3d3
Xyrex Kyle Salazar <122286060+xyxyxyrex@users.noreply.github.com>
2023-11-27 09:31:12 +0800

    login/register API

--- login.php
+++ login.php
@@ -7,27 +7,41 @@ $dbname = "thriftlyapp_db";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo json_encode(["error" => "Connection failed"]);
+    exit();
+}
+
+// Check if the required keys are set in the $_POST array
+if (!isset($_POST['username'], $_POST['password'])) {
+    echo json_encode(["error" => "Missing POST parameters"]);
+    exit();
 }
 
 $username = mysqli_real_escape_string($conn, $_POST['username']);
 $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-$sql = "SELECT * FROM users WHERE username='$username'";
-$result = $conn->query($sql);
+$stmt = $conn->prepare("SELECT * FROM users WHERE username=?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$response = [];
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    $hashed_password = $row['password_hash'];
+    $stored_password = $row['password'];
 
-    // Use password_verify to check if the entered password matches the hashed password
-    if (password_verify($password, $hashed_password)) {
-        echo "Login successful";
+    // Trim whitespace and compare
+    if (trim($password) === trim($stored_password)) {
+        $response["message"] = "Login successful";
     } else {
-        echo "Login failed";
+        $response["error"] = "Password mismatch: " . $password . " vs " . $stored_password;
     }
 } else {
-    echo "Login failed";
+    $response["error"] = "User not found";
 }
 
+$stmt->close();
 $conn->close();
+
+echo json_encode($response);
--- register.php
+++ register.php
@@ -6,38 +6,39 @@ $dbname = "thriftlyapp_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check for database connection errors
 if ($conn->connect_error) {
-    echo "Error: Connection failed";
+    echo json_encode(["error" => "Connection failed"]);
+    exit();
+}
+
+// Check if the required keys are set in the $_POST array
+if (!isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['mobile_number'])) {
+    echo json_encode(["error" => "Missing POST parameters"]);
     exit();
 }
 
 $username = mysqli_real_escape_string($conn, $_POST['username']);
 $email = mysqli_real_escape_string($conn, $_POST['email']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
-$confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
+$password = mysqli_real_escape_string($conn, $_POST['password']); // Don't hash the password
 $mobile_number = mysqli_real_escape_string($conn, $_POST['mobile_number']);
 
-// Check if passwords match
-if ($password !== $confirm_password) {
-    echo "Passwords do not match";
-    exit();
-}
+// Use prepared statements to prevent SQL injection
+$stmt = $conn->prepare("INSERT INTO users (username, email, password, mobile_number) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $username, $email, $password, $mobile_number);
 
-// Hash the password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+$response = [];
 
-$sql = "INSERT INTO users (username, email, password_hash, mobile_number) VALUES ('$username', '$email', '$hashed_password', '$mobile_number')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Registration successful";
+if ($stmt->execute()) {
+    $response["message"] = "Registration successful";
 } else {
-    // Check for duplicate entry error
     if (strpos($conn->error, "Duplicate entry") !== false) {
-        echo "Error: Email already registered";
+        $response["error"] = "Email already registered";
     } else {
-        echo "Error: Registration failed";
+        $response["error"] = "Registration failed";
     }
 }
 
+$stmt->close();
 $conn->close();
+
+echo json_encode($response);
